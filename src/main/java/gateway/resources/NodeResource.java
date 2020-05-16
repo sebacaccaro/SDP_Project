cb65406e58cdfc107f66dcd168b0be8ff5b4ba12
sebacaccaro <sebastianocaccaro@gmail.com>
2020-05-16 17:10:20 +0200

    Node deletion implementation

--- src/main/java/gateway/resources/NodeResource.java
+++ src/main/java/gateway/resources/NodeResource.java
@@ -1,16 +1,14 @@
 package gateway.resources;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import gateway.ConcurrentStructures.DuplicateKeyException;
 import gateway.store.Store;
 import gateway.store.beans.NodeBean;
-import gateway.store.beans.NodeBeanList;
 
 /**
  * Resource handler for single nodes talking to the gateway
@@ -28,14 +26,32 @@ public class NodeResource {
     @POST
     @Path("/join")
     @Produces({ "application/json" })
-    public Response join(NodeBean joiningNode) throws InterruptedException {
+    public Response join(NodeBean joiningNode){
         Store s = new Store();
         try {
             s.addNode(joiningNode);
+            return Response.ok(s.getNodes()).build();
         } catch (DuplicateKeyException e) {
             // 409 Conflict: The user can change and repeat the request
             return Response.status(409).entity(e.getMessage()).build();
-        }
-        return Response.ok(s.getNodes()).build();
+        } catch (InterruptedException e) {
+            // 500 Internal Server Error
+            return Response.status(500).build();
+        }        
     }
+
+    @DELETE
+    @Path("/leave")
+    @Produces({ "application/json" })
+    public Response leave(NodeBean leavingNode) {
+        Store s = new Store();
+        try {
+            s.removeNode(leavingNode.getId());
+        } catch (InterruptedException e) {
+            // 500 Internal Server Error
+            return Response.status(500).build();
+        }
+        return Response.ok().build();
+    }
+
 }
